70d1edb1385b742206be080b5ba179da89a39a70
amy-chinlai <amy@actionnetwork.org>
2021-12-13 14:09:25 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("The DOM has loaded");
+    // fetch(imgURL)
+    // .then (function(response) {
+    //     return response.json()
+    // })
+    // .then(function(json) {
+    //     for (const element of json[message]) {
+    //         console.log(element)
+    //     }
+    // })
+});
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+console.log("here it is")
--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 console.log('%c HI', 'color: firebrick')
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("The DOM has loaded");
+    fetch(imgUrl)
+    .then (function(response) {
+        return response.json()
+    })
+    .then(function(json) {
+        for (const element of json.message) {
+            console.log(element)
+            appendImage(element)
+        }
+    })
+});
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+
+
+const main = document.querySelector('body')
+
+function appendImage(e) {
+    var img = document.createElement('img');
+    img.src = e
+    document.querySelector('body').appendChild(img)
+}
